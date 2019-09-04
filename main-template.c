c3c4c8ad74b1b31dc4af1d04d4e749308620d998
arosilier <50966934+arosilier@users.noreply.github.com>
2019-09-03 21:46:24 -0500

    Add files via upload

---
+++ main-template.c
@@ -0,0 +1,22 @@
+// ----------------------------------------------------
+// Name:                                              
+// Class: EG1302B Fall 2019                     
+// Assignment #:                                      
+// Due Date:                                          
+//                                                    
+// Description of program here...                     
+//                                                     
+
+// Preprocessor directives
+#include <stdio.h> 
+
+
+// Main function
+int main()
+{
+
+
+  // Exit program
+  return 0;
+}
+// ----------------------------------------------------
